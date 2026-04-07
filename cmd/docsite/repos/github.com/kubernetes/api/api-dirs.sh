eb412ed9c3d3b4080e3c45acd54e58dbdf79ed35
Bowei Du <bowei.du@gmail.com>
2026-04-07 15:06:43 -0700

    update api dirs for K8s

--- cmd/docsite/repos/github.com/kubernetes/api/api-dirs.sh
+++ cmd/docsite/repos/github.com/kubernetes/api/api-dirs.sh
@@ -1,4 +1,28 @@
 #!/bin/bash
 
-echo "core"
-echo "network"
+cat <<EOF 
+admission
+admissionregistration
+apidiscovery
+apiserverinternal
+apps
+authentication
+authorization
+autoscaling
+batch
+certificates
+coordination
+core
+discovery
+events
+extensions
+flowcontrol
+imagepolicy
+networking
+node
+policy
+rbac
+resource
+storage
+storagemigration
+EOF
